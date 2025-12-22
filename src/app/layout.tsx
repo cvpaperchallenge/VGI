a87bdceecdeae3a1637a71127bd438469a230d4e
gatheluck <gatheluck@gmail.com>
2025-12-23 00:28:31 +0900

    feat: make header sticky.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export default function SharedUILayout() {
     <div className="min-h-screen bg-gradient-to-br from-background from-20% via-background-gradation-1 via-50% to-background-gradation-2 to-90%">
       <div className="bg-animated simple-grid flex min-h-screen w-full flex-col  items-center overflow-x-hidden">
         <Header />
-        <div className="grow">
+        <div className="grow pt-16">
           <Outlet />
         </div>
         <Footer />
